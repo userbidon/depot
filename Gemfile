278b22997d171b169959c1a2a88dc68fb659da9f
userbidon <userbidon@yopmail.com>
2015-03-05 10:44:05 +0100

    blablabla

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'haml'
 gem "haml-rails", "~> 0.8"
 gem 'mysql2'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_share_variable
+
+  protected
+
+  def set_share_variable
+    @time = Time.now
+  end
+
+  private
+
+  def current_cart
+    Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
+
 end
